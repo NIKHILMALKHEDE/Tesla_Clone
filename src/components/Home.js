0cbd5ae22389d606b0d5b22bb3f27379a9b0998f
NIKHILMALKHEDE <malkhedenikhil@gmail.com>
2022-02-14 21:25:33 +0530

    tesla clone

---
+++ src/components/Home.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import styled from 'styled-components';
+import Section from './Section';
+
+function Home() {
+  return (
+      <Container>
+         <Section 
+            title="Model S"
+            description="Order Online for Touchless Delivery"
+            backgroundImg="model-s.jpg"
+            leftBtnText="Custom Order"
+            rightBtnText="Existing Invetory"
+         />
+         <Section 
+            title="Model Y"
+            description="Order Online for Touchless Delivery"
+            backgroundImg="model-y.jpg"
+            leftBtnText="Custom Order"
+            rightBtnText="Existing Invetory"
+         />
+         <Section 
+            title="Model 3"
+            description="Order Online for Touchless Delivery"
+            backgroundImg="model-3.jpg"
+            leftBtnText="Custom Order"
+            rightBtnText="Existing Invetory"
+         />
+         <Section 
+            title="Model X"
+            description="Order Online for Touchless Delivery"
+            backgroundImg="model-x.jpg"
+            leftBtnText="Custom Order"
+            rightBtnText="Existing Invetory"
+         />
+         <Section 
+            title="Lowest Cost Solar Panels in India"
+            description="Money-back gurantee"
+            backgroundImg="solar-panel.jpg"
+            leftBtnText="Order Now"
+            rightBtnText="Learn More"
+         />
+         <Section 
+            title="Solar for New Roofs"
+            description="Solar Roof Cost Less Than a New Roof Plus Solar Panels"
+            backgroundImg="solar-roof.jpg"
+            leftBtnText="Order Now"
+            rightBtnText="Learn More"
+         />
+         <Section 
+            title="Accessories"
+            description=""
+            backgroundImg="accessories.jpg"
+            leftBtnText="Shop Now"
+         />
+      </Container>
+  )
+}
+
+export default Home;
+
+const Container = styled.div`
+   height: 100vh;
+`
